a49e04b348cc87008c0949b30aab1110a6535c10
vinklarik.peter <vinklarik.peter@28-DIAK3>
2024-10-07 14:38:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using D241007_13B_Vinklárik_Péter_CS3;
+using System.Text;
+
+List<olvas> Versenyzők = [];
+
+using StreamReader sr = new(
+    path: @"..\..\..\src\forras.txt",
+    encoding: Encoding.UTF8);
+_ = sr.ReadLine();
+while (!sr.EndOfStream)
+{
+    Versenyzők.Add(new(sr.ReadLine()));
+}
+Console.WriteLine($"1.feladat: {Versenyzők.Count}");
+/* Console.WriteLine($"2.feladat: 25 és 29 éves korú versenyzők száma: {Versenyzők}"); */
---
+++ olvas.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D241007_13B_Vinklárik_Péter_CS3
+{
+    internal class olvas
+    {
+        public string Nev { get; set; }
+        public int SzulEv { get; set; }
+        public int Rajtszam { get; set; }
+        public string FvN {  get; set; }
+        public string Eletkor { get; set; }
+        public Dictionary <string, DateTime> Idoeredmeny { get; set; }
+
+        public override string ToString()
+        {
+            return $"\tNev: {Nev}\n" +
+                $"\tSzulEv: {SzulEv}" +
+                $"\tRajtszam: {Rajtszam}" +
+                $"\tFérfi vagy Nő: {FvN}" +
+                $"\tÉletkor: {Eletkor}" +
+                $"\tIdoeredmények: "+
+                $"a: {Idoeredmeny["úszás ideje"]}"+
+                $"b: {Idoeredmeny["első depóba töltött idő"]}"+
+                $"c: {Idoeredmeny["kerékpározás ideje"]}"+
+                $"d: {Idoeredmeny["második depóba töltött idő"]}"+
+                $"e: {Idoeredmeny["futás ideje"]}";
+        }
+        public olvas(string row) 
+        {
+            var v = row.Split(';');
+             Nev = v[0];
+            SzulEv = int.Parse(v[1]);
+            Rajtszam = int.Parse(v[2]);
+            FvN = v[3];
+            Eletkor = v[4];
+            Idoeredmeny = new()
+            {
+                { "úszás ideje", DateTime.Parse(v[5])},
+                { "első depóba töltött idő", DateTime.Parse(v[6])},
+                { "kerékpározás ideje", DateTime.Parse (v[7])},
+                {"második depóban töltött idő", DateTime.Parse(v[8])},
+                {"futás ideje", DateTime.Parse(v[9])},
+            };
+        }
+    }
+}
